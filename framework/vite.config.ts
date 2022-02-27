b79ba0a117c8669666821b7af80039dffdc5d063
Romuald Brillout <git@brillout.com>
2022-02-27 17:06:58 +0100

    wip

--- framework/vite.config.ts
+++ framework/vite.config.ts
@@ -1,15 +1,54 @@
-import react from '@vitejs/plugin-react'
-import ssr from 'vite-plugin-ssr/plugin'
-import type { UserConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import ssr from "vite-plugin-ssr/plugin";
+import type { UserConfig, Plugin } from "vite";
+import { resolve } from "path";
+
+const r = (p) => resolve(__dirname, p);
 
 const config: UserConfig = {
-  plugins: [react(), ssr()],
-  build: {
-    rollupOptions: {
-      input: {
-        'vite-plugin-ssr.config': require.resolve('./vite-plugin-ssr.config'),
-        'components': require.resolve('./components')
+  plugins: [react(), ssr(), frameworkBuilder()],
+};
+export default config;
+
+function frameworkBuilder(): Plugin {
+  return {
+    name: "vps-framework-builder",
+    apply: "build",
+    config: (config) => {
+      const isSSR = !!config.build.ssr;
+      if (isSSR) {
+        return configServer();
+      } else {
+        return {
+          esbuild,
+        };
+      }
+    },
+  };
+}
+
+function configClient(): UserConfig {
+  return {
+    build: {
+      rollupOptions: {
+        external: require("./package.json").dependencies,
       },
+    },
+    esbuild: {
+      minify: false,
+    },
+  };
+}
+function configServer(): UserConfig {
+  return {
+    build: {
+      rollupOptions: {
+        input: {
+          "vite-plugin-ssr.config": r("./vite-plugin-ssr.config"),
+          components: r("./components"),
+          //@ts-ignore
+          //'components': await import.meta.resolve('./components')
+        },
         /*
       output: {
         //entryFileNames: '[name].js',
@@ -21,7 +60,7 @@ const config: UserConfig = {
         },
       }
         //*/
-    }
-  }
+      },
+    },
+  };
 }
-export default config
